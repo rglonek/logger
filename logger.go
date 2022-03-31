2dd0d2f07b59c307e434650479cbd70b9ce30a37
rglonek <97889953+rglonek@users.noreply.github.com>
2022-03-31 17:35:04 +0100

    allow for named loggers

--- logger.go
+++ logger.go
@@ -5,65 +5,110 @@ import (
 	"os"
 )
 
-var logLevel = 4 // 0=NO_LOGGING 1=CRITICAL, 2=ERROR, 3=WARNING, 4=INFO, 5=DEBUG, 6=DETAIL
-
-var p = ""
-
-func SetPrefix(prefix string) {
-	p = prefix
-}
-
-func SetLogLevel(level int) {
-	if level < 0 {
-		level = 0
-	}
-	logLevel = level
-}
-
 func Info(format string, v ...interface{}) {
-	if logLevel < 4 {
-		return
-	}
-	format = "INFO " + p + format
-	log.Printf(format, v...)
+	defaultLogger.Info(format, v)
 }
 
 func Warn(format string, v ...interface{}) {
-	if logLevel < 3 {
-		return
-	}
-	format = "WARNING " + p + format
-	log.Printf(format, v...)
+	defaultLogger.Warn(format, v)
 }
 
 func Error(format string, v ...interface{}) {
-	if logLevel < 2 {
-		return
-	}
-	format = "ERROR " + p + format
-	log.Printf(format, v...)
+	defaultLogger.Error(format, v)
 }
 
 func Critical(format string, v ...interface{}) {
-	if logLevel >= 1 {
-		format = "CRITICAL " + p + format
+	defaultLogger.Critical(format, v)
+}
+
+func Debug(format string, v ...interface{}) {
+	defaultLogger.Debug(format, v)
+}
+
+func Detail(format string, v ...interface{}) {
+	defaultLogger.Detail(format, v)
+}
+
+func SetPrefix(prefix string) {
+	defaultLogger.SetPrefix(prefix)
+}
+
+func SetLogLevel(level int) {
+	defaultLogger.SetLogLevel(level)
+}
+
+func NewLogger() *Logger {
+	return &Logger{
+		logLevel: defaultLogger.logLevel,
+		p:        defaultLogger.p,
+	}
+}
+
+type Logger struct {
+	logLevel int
+	p        string
+}
+
+var defaultLogger = &Logger{
+	logLevel: 4, // 0=NO_LOGGING 1=CRITICAL, 2=ERROR, 3=WARNING, 4=INFO, 5=DEBUG, 6=DETAIL
+	p:        "",
+}
+
+func (l *Logger) SetPrefix(prefix string) {
+	l.p = prefix
+}
+
+func (l *Logger) SetLogLevel(level int) {
+	if level < 0 {
+		level = 0
+	}
+	l.logLevel = level
+}
+
+func (l *Logger) Info(format string, v ...interface{}) {
+	if l.logLevel < 4 {
+		return
+	}
+	format = "INFO " + l.p + format
+	log.Printf(format, v...)
+}
+
+func (l *Logger) Warn(format string, v ...interface{}) {
+	if l.logLevel < 3 {
+		return
+	}
+	format = "WARNING " + l.p + format
+	log.Printf(format, v...)
+}
+
+func (l *Logger) Error(format string, v ...interface{}) {
+	if l.logLevel < 2 {
+		return
+	}
+	format = "ERROR " + l.p + format
+	log.Printf(format, v...)
+}
+
+func (l *Logger) Critical(format string, v ...interface{}) {
+	if l.logLevel >= 1 {
+		format = "CRITICAL " + l.p + format
 		log.Printf(format, v...)
 	}
 	os.Exit(1)
 }
 
-func Debug(format string, v ...interface{}) {
-	if logLevel < 5 {
+func (l *Logger) Debug(format string, v ...interface{}) {
+	if l.logLevel < 5 {
 		return
 	}
-	format = "DEBUG " + p + format
+	format = "DEBUG " + l.p + format
 	log.Printf(format, v...)
 }
 
-func Detail(format string, v ...interface{}) {
-	if logLevel < 6 {
+func (l *Logger) Detail(format string, v ...interface{}) {
+	if l.logLevel < 6 {
 		return
 	}
-	format = "DETAIL " + p + format
+	format = "DETAIL " + l.p + format
 	log.Printf(format, v...)
 }
